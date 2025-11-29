5ce047653f2df41520a85cb0835ba1b62fc3337b
javidominguezsegura <javier.dominguez.segura@gmail.com>
2025-11-29 23:25:22 +0100

    finally all CD code

--- backend/src/url_shortener/routers/health.py
+++ backend/src/url_shortener/routers/health.py
@@ -61,4 +61,4 @@ async def cheeck_backend_health_dependencies_endpoint(
 async def cheeck_backend_health_endpoint(
 	app_settings: Annotated[Settings, Depends(get_app_settings)],
 ) -> Dict:
-	return {"response": "pong (1ST DEPLOYMENT)"}
+	return {"response": "pong (2ND DEPLOYMENT) -- IM FINALLY WORKING"}
